0c9f325cfeffcc763c4bb0f2e6810e275b31c5be
Abhabhish <agmishra241270@gmail.com>
2024-04-03 17:39:14 +0530

    adf

--- searchapp/views.py
+++ searchapp/views.py
@@ -11,6 +11,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 import time
 import json
 
