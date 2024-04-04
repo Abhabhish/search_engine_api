0bea94971d4612c4bcbf18e9bef11dc2a5e8c602
Abhabhish <agmishra241270@gmail.com>
2024-04-04 10:33:06 +0530

    adfs

--- searchapp/views.py
+++ searchapp/views.py
@@ -21,13 +21,13 @@ def all(img_url):
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    # options.add_argument('--headless')  # Run in headless mode
+    options.add_argument('--headless')  # Run in headless mode
     options.add_argument('--no-sandbox')  # Bypass OS security model
     options.add_argument('--disable-dev-shm-usage')
 
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    wait=WebDriverWait(driver,30)
+    wait=WebDriverWait(driver,15)
 
     def bing(img_url):
         driver.get(f'https://www.bing.com/images/searchbyimage?cbir=ssbi&imgurl={img_url}')
