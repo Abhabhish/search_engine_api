34d8e21d5df2092c3ebb07c2233ad90ef2d7fa69
Abhabhish <agmishra241270@gmail.com>
2024-04-06 14:20:26 +0530

    sf

--- search_engine_api/urls.py
+++ search_engine_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('image/',include('searchapp.urls'))
+    path('search/',include('searchapp.urls'))
 ]
